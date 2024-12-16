19a35bbe9acc4dad37936053ea30263d7ccb37a5
ironsummit72 <ironsumit72@gmail.com>
2024-12-16 15:01:11 +0530

    added cors in streaming service

--- videoStreamingService/index.js
+++ videoStreamingService/index.js
@@ -2,6 +2,7 @@ import { configDotenv } from "dotenv";
 import express from 'express'
 import connectDatabase from "./utils/connectDb.util.js";
 import videoRouter from "./routes/videos.routes.js";
+import cors from 'cors'
 configDotenv({
   path: "./.streamingService.env",
 });
@@ -11,7 +12,7 @@ connectDatabase();
 const app=express();
 const port=process.env.PORT;
 
-
+app.use(cors())
 app.use('/video',videoRouter)
 
 
@@ -23,3 +24,5 @@ app.use('/video',videoRouter)
 app.listen(port,()=>{
 console.log(`Streaming service is running on port ${port}`);
 })
+
+
